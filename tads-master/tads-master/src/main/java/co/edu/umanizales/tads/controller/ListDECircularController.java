2c5729982fbca32f6b4c80e60e4cb2af99fb45bf
DanielCortes16 <127440018+DanielCortes16@users.noreply.github.com>
2023-05-25 10:05:28 -0500

    Perro con mas pulgaas

--- tads-master/tads-master/src/main/java/co/edu/umanizales/tads/controller/ListDECircularController.java
+++ tads-master/tads-master/src/main/java/co/edu/umanizales/tads/controller/ListDECircularController.java
@@ -13,8 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 @RestController
@@ -74,6 +73,6 @@ public class ListDECircularController {
         FleasDTO fleasDTO = new FleasDTO();
         fleasDTO.setName(pet.getName());
         fleasDTO.setFleas(pet.getFleas());
-        return ResponseEntity.ok(new ResponseDTO(200, fleasDTO, null));
+        return new ResponseEntity<>(new ResponseDTO(200, fleasDTO, null), HttpStatus.OK);
     }
 }
